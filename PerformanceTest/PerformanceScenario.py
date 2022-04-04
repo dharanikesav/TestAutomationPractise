a058e2877b9946cdd0b4b56bc106eed6ce5c67ee
dharanikesav <dharanikesav@gmail.com>
2022-04-04 06:56:04 +0200

    Update website for performance test

--- PerformanceTest/PerformanceScenario.py
+++ PerformanceTest/PerformanceScenario.py
@@ -1,11 +1,11 @@
-from locust import HttpUser, TaskSet, task
+from locust import HttpUser, TaskSet, task, between
 
 
 class BlazeDemoTask(TaskSet):
 
     @task
     def getHomePage(self):
-        self.client.get(url='/en')
+        self.client.get(url='/index.php')
 
     def on_stop(self):
         pass
@@ -13,4 +13,5 @@ class BlazeDemoTask(TaskSet):
 
 class Website(HttpUser):
     tasks = [BlazeDemoTask]
-    host = 'https://www.bitpanda.com'
+    host = 'https://blazedemo.com'
+    wait_time = between(0.3, 0.5)
